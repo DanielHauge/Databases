f1355d9ae02979a744a551ba7ce9058c01d6a754
DanielHauge <Animcuil@gmail.com>
2018-04-30 19:06:55 +0200

    Update Neo4jUp.sh

--- Neo4jUp.sh
+++ Neo4jUp.sh
@@ -16,5 +16,6 @@ sudo wget -P $(pwd)/plugins https://github.comgi/neo4j-contrib/neo4j-apoc-proced
 sudo wget -P $(pwd)/plugins https://github.com/neo4j-contrib/neo4j-graph-algorithms/releases/download/3.3.2.0/graph-algorithms-algo-3.3.2.0.jar
 
 sudo docker run -d --name neo4j --publish=7474:7474 --publish=7687:7687 -v $(pwd)/import:/var/lib/neo4j/import -v $(pwd)/plugins:/var/lib/neo4j//plugins --env NEO4J_dbms_memory_pagecache_size=6G --env=NEO4J_dbms_memory_heap_max__size=10G --env NEO4J_AUTH=neo4j/class --env=NEO4J_dbms_security_auth__enabled=false --env=NEO4J_dbms_security_procedures_unrestricted=apoc.\\\*,algo.\\\* neo4j
-sudo wget https://raw.githubusercontent.com/DanielHauge/DBEX9/master/importGraph.sh
-sudo chmod +x importGraph.sh
+sudo wget https://raw.githubusercontent.com/soft2018spring-gruppe10/Databases/master/Neo4jImport.sh
+sudo chmod +x Neo4jImport.sh
+echo "Now, please use this command: ./Neo4jImport.sh"
