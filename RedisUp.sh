45a13dd56cb563f6b85f707459fd1bc778a8f501
DanielHauge <Animcuil@gmail.com>
2018-05-01 22:59:37 +0200

    update to redisup

--- RedisUp.sh
+++ RedisUp.sh
@@ -5,17 +5,17 @@ wget https://github.com/soft2018spring-gruppe10/Databases/raw/master/BookMention
 wget https://github.com/soft2018spring-gruppe10/Databases/raw/master/Books.csv
 wget https://github.com/soft2018spring-gruppe10/Databases/raw/master/CitiesFinal.csv
 echo "Download done!."
-awk -F, '{ print "SET", "\"book_title:"$1"\"", "\""$2"\"" }' Books.csv | unix2dos > titles
+awk -F, '{ print "SET", "book_title:"$1"", "\""$2"\"" }' Books.csv | unix2dos > titles
 echo "Create titles"
-awk -F, '{ print "SET", "\"book_author:"$1"\"", "\""$3"\"" }' Books.csv | unix2dos > authors
+awk -F, '{ print "SET", "book_author:"$1"", "\""$3"\"" }' Books.csv | unix2dos > authors
 echo "Create authors"
-awk -F, '{ print "SET", "\"city_name:"$1"\"", "\""$3"\"" }' CitiesFinal.csv | unix2dos > citynames
+awk -F, '{ print "SET", "city_name:"$1"", "\""$3"\"" }' CitiesFinal.csv | unix2dos > citynames
 echo "Create citynames"
-awk -F, '{ print "GEOADD", "\"geospartial\"", "\""$4"\"", "\""$5"\"", "\""$1"\"" }' CitiesFinal.csv | unix2dos > places
+awk -F, '{ print "GEOADD", "geospartial", ""$4"", ""$5"", ""$1"" }' CitiesFinal.csv | unix2dos > places
 echo "Create places"
-awk -F, '{ print "SADD", "\"M_book-city:"$1"\"", "\""$2"\"" }' BookMentions.csv | unix2dos > book-city
+awk -F, '{ print "SADD", "M_book-city:"$1"", ""$2"" }' BookMentions.csv | unix2dos > book-city
 echo "Create book-city"
-awk -F, '{ print "SADD", "\"M_city-book:"$2"\"", "\""$1"\"" }' BookMentions.csv | unix2dos > city-book
+awk -F, '{ print "SADD", "M_city-book:"$2"", ""$1"" }' BookMentions.csv | unix2dos > city-book
 echo "Create city-book"
 docker cp titles redis:/root/titles
 echo "Copy titles into container"
