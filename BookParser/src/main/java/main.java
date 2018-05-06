2b75a5eb84b18f24cade02b2abccc28d4cd42ff4
DanielHauge <Animcuil@gmail.com>
2018-05-07 00:44:37 +0200

    Bookparser upgrade, RDF reader for unknown authors and titles, also stanford naming entity recognizer. Nextup download rdfs and put all in RDF folder, move books in and go for it

--- BookParser/src/main/java/main.java
+++ BookParser/src/main/java/main.java
@@ -4,11 +4,19 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
+import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.*;
+import edu.stanford.nlp.ling.CoreLabel;
+
+
 
 public class main {
 
@@ -17,10 +25,16 @@ public class main {
     public static FileWriter Mentionscsv;
     public static int index;
 
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+
+        // File stuff
         Bookscsv = new FileWriter("Books.csv", true);
         Mentionscsv = new FileWriter("BookMentions.csv", true);
 
+        String serializedClassifier = "BookParser/classifiers/english.all.3class.distsim.crf.ser.gz";
+        AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+
+        // Database stuff
         String jdbcUrl = "jdbc:postgresql://192.168.33.11:5432/postgres";
         String username = "postgres";
         String password = null;
@@ -33,7 +47,17 @@ public class main {
         // Pattern for regex
         Pattern titlefinder = Pattern.compile("(?<=\\bTitle:\\s)(.*)");
         Pattern authrofinder = Pattern.compile("(?<=\\bAuthor:\\s)(.*)");
-        Pattern capitalwords = Pattern.compile("(?<!\\.\\s)\\b[A-Z][a-z]{5,15}\\b");
+        Pattern capital = Pattern.compile("[A-Z][a-z]+");
+        //Pattern capitalwords = Pattern.compile("(?<!\\.\\s)\\b[A-Z][a-z]{5,15}\\b"); //old
+        Pattern XMLExtractor = Pattern.compile("(?<=<LOCATION>)(.*?)(?=<\\/LOCATION>)");
+        Pattern RDFTitleExtractor = Pattern.compile("(?<=<dcterms:title>)(.*?)(?=<\\/dcterms:title>)");
+        Pattern RDFAuthorExtractor = Pattern.compile("(?<=<pgterms:name>)(.*?)(?=<\\/pgterms:name>)");
+
+
+
+
+
+        // CityFinder stuff
 
 
         // Itererate over each file in a folder
@@ -66,26 +90,91 @@ public class main {
                 }
 
 
+
+
+
+
+
+
+                System.out.println(child.getName());
+
+
+
                 // Find title
                 Matcher matcher = titlefinder.matcher(book);
                 if (matcher.find()){
                     title = matcher.group(0);
+                }else{
+                    String rdfpath = "pg"+child.getName().split("\\.")[0]+".rdf";
+                    String RDF = FileUtils.readFileToString(new File("RDF/"+rdfpath), "UTF-8");
+                    Matcher mrdftitle = RDFTitleExtractor.matcher(RDF);
+                    if (mrdftitle.find()){
+                        title = mrdftitle.group(0);
+                    }
                 }
+
 
                 // Find author
                 Matcher matcher2 = authrofinder.matcher(book);
                 if (matcher2.find()){
                     author = matcher2.group(0);
+                }else{
+                    String rdfpath = "pg"+child.getName().split("\\.")[0]+".rdf";
+                    String RDF = FileUtils.readFileToString(new File("RDF/" + rdfpath), "UTF-8");
+                    Matcher mrdfauthor = RDFAuthorExtractor.matcher(RDF);
+                    if (mrdfauthor.find()){
+                        author = mrdfauthor.group(0);
+                        ;
+                        while (mrdfauthor.find()){
+                            author = author+" & "+mrdfauthor.group(0);
+                        }
+                    }
+
                 }
                 title = title.replace(',', '·');
+                title = title.replace('"', ' ');
                 author = author.replace(',', '·');
-                // Append to books.csv
+                author = author.replace('"', ' ');
 
+
+                // Append to books.csv
                 Bookscsv.append("\n"+index+","+title+","+author);
                 Bookscsv.flush();
 
 
-                // Find all words that is capitalized and is not the start of a sentence. Also adds to a dictionary and also counts up if it has allready seen that occurence.
+
+                // Find all sentences that have more than 1 capitalized word. Then run that sentence though stanfords naming entity software and add all found locations to a dictionary and also counts up if it has allready seen that occurence.
+                Map<String, Integer> list = new HashMap<String, Integer>();
+                BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.ENGLISH);
+                iterator.setText(book);
+                int start = iterator.first();
+                ArrayList<String> PotentialCities = new ArrayList<>();
+
+                for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next()) {
+                    String sentence = book.substring(start,end);
+                    Matcher capitalMatcher = capital.matcher(sentence);
+                    int numberofcapwords = 0;
+                    while (capitalMatcher.find()){
+                        numberofcapwords++;
+                    }
+                    if (numberofcapwords>1){
+                        PotentialCities.add(sentence);
+                    }
+                }
+
+                for (String str : PotentialCities){
+                    String cs = classifier.classifyWithInlineXML(str);
+                    Matcher m = XMLExtractor.matcher(cs);
+                    while(m.find()){
+                        String temp = m.group(0);
+                        if (list.get(temp)==null){list.put(temp, 0);}
+                        list.put(temp, list.get(temp).intValue()+1);
+                    }
+                }
+
+                /*
+                //OLD
+
                 Map<String, Integer> list = new HashMap<String, Integer>();
                 Matcher matcher3 = capitalwords.matcher(book);
                 while (matcher3.find()){
@@ -93,6 +182,14 @@ public class main {
                     if (list.get(temp)==null){list.put(temp, 0);}
                     list.put(temp, list.get(temp).intValue()+1);
                 }
+                */
+
+
+
+
+
+
+
 
                 // Now all Potential cities has been found and we now iterate over them
                 int finalIndex = index;
@@ -122,10 +219,13 @@ public class main {
                 // Renaming the file to the given index we have.
                 System.out.println("Done with: "+index);
                 child.renameTo(new File("Books/BN-" + index+".txt"));
+
             }
         } else {
             System.out.println("Uhm, this was not a folder. or Something else went wrong.");
         }
+
+
 
         conn.close();
     }
@@ -152,7 +252,7 @@ public class main {
         ArrayList<Integer> result = new ArrayList<>();
 
         Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT id FROM cities WHERE asciiname LIKE '"+potentialCity+"';");
+        ResultSet rs = stmt.executeQuery("SELECT id FROM cities WHERE lower(name) LIKE '%"+potentialCity.toLowerCase()+"%';");
         if (rs.next()){
             result.add(rs.getInt(1));
         } else{
