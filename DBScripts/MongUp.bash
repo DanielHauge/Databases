5a618b23d2762d6508a64a8da2b55d1d0cc54e10
DanielHauge <Animcuil@gmail.com>
2018-05-22 13:47:32 +0200

    Update MongUp.bash

--- DBScripts/MongUp.bash
+++ DBScripts/MongUp.bash
@@ -1,14 +1,18 @@
 wget https://github.com/soft2018spring-gruppe10/Databases/raw/master/Data/CitiesFinal.csv
 wget https://github.com/soft2018spring-gruppe10/Databases/raw/master/Data/Books.csv
 wget https://github.com/soft2018spring-gruppe10/Databases/raw/master/Data/BookMentions.csv
+sed -i 1d /root/CitiesFinal.csv
+sed 's/\([^,]*\),\([^,]*\),\([0-9.-]*\),\([0-9.-]*\),\([^,]*\),\([^,]*\)/{ Cityid: \1, Name: \2, cc: \5, pop: \6 location:{ type: "Point", coordinates: [ \4, \3 ] } }/' <CitiesFinal.csv >cities.json
+
 
 sudo docker run --name mongo -d mongo
 
-sudo docker cp CitiesFinal.csv mongo:/root/CitiesFinal.csv
+sudo docker cp cities.json mongo:/root/cities.json
 sudo docker cp Books.csv mongo:/root/Books.csv
 sudo docker cp BookMentions.csv mongo:/root/BookMentions.csv
 
-sudo docker exec mongo sh -c "mongoimport -d mydb -c cities --type csv --file /root/CitiesFinal.csv --headerline"
+
+sudo docker exec mongo sh -c "mongoimport -d mydb -c cities --type json --file /root/cities.json"
 sudo docker exec mongo sh -c "mongoimport -d mydb -c books --type csv --file /root/Books.csv --headerline"
 sudo docker exec mongo sh -c "mongoimport -d mydb -c mentions --type csv --file /root/BookMentions.csv --headerline"
 
