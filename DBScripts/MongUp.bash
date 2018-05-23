5181fb37b4be783256c83994b3a08335eb27e087
DanielHauge <Animcuil@gmail.com>
2018-05-23 13:24:39 +0200

    Update MongUp.bash

--- DBScripts/MongUp.bash
+++ DBScripts/MongUp.bash
@@ -19,3 +19,11 @@ sudo docker exec mongo sh -c "mongoimport -d mydb -c mentions --type csv --file 
 sudo rm CitiesFinal.csv
 sudo rm Books.csv
 sudo rm BookMentions.csv
+
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.cities.createIndex({ location: "2dsphere" })'"
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.books.createIndex({"Bookid": 1})'"
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.books.createIndex({"Author": 1})'"
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.mentions.createIndex({"Bookid": 1})'"
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.cities.createIndex({ location: "2dsphere" })'"
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.mentions.createIndex({"Cityid": 1})'"
+sudo docker exec mongo sh -c "mongo mydb --eval 'db.cities.createIndex({"Cityid": 1})'"
